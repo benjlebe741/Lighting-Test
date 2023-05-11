6d88b609141c1eea28bebeb4b992a5d9c4ee3d7f
benjlebe741 <benjlebe741@ed.amdsb.ca>
2023-05-11 18:41:45 -0400

    LEVELS HAVE BEEN REVAMPED. Instead of multiple lists corresponding to each level, only one list exists with a series of instances of the level class. Lights can now be stored in levels including player light

--- Lighting Test/Level.cs
+++ Lighting Test/Level.cs
@@ -11,16 +11,12 @@ namespace Lighting_Test
         public List<Light> lights = new List<Light>();
         public List<int> tileSprites = new List<int>();
         public List<int> depths = new List<int>();
-        public int width;
-        public int height;
 
-        public Level(int _width, List<int> _tileSprites, List<int> _depths, List<Light> _lights) 
+        public Level(List<int> _tileSprites, List<int> _depths, List<Light> _lights) 
         {
             lights = _lights;
             tileSprites = _tileSprites;
             depths = _depths;
-            width = _width;
-            height = _depths.Count / _width;
         }
     }
 }
